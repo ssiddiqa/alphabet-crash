239564054ba59e497687a6f70d88aec320cc9e63
ssiddiqa <siddiqasayma@gmail.com>
2024-08-14 16:25:05 +0600

    done

--- Js/connect.js
+++ Js/connect.js
@@ -1,4 +1,48 @@
 function letsPlay() {
     hideElement('home')
+    hideElement('finalRes')
     showElement('playGround')
+    setValue('life', 5)
+    setValue('score', 0)
+    startGame()
+}
+function gameOver() {
+    hideElement('playGround')
+    showElement('finalRes')
+    const score = document.getElementById('score').innerText
+    setValue('result-final', score)
+    const current = document.getElementById('screen').innerText
+    removeBgColor(current)
+}
+function handleKeyUpEvent(event) {
+    const playerKey = event.key
+    if (playerKey == 'Escape') {
+        gameOver()
+    }
+    const currentAlphabet = document.getElementById('screen')
+    const currentVal = currentAlphabet.innerText;
+    const expected = currentVal.toLowerCase()
+    if (playerKey == expected) {
+        const currentScore = getValue('score')
+        const newScore = currentScore + 1
+        setValue('score', newScore)
+        removeBgColor(expected)
+        startGame()
+    } else {
+        const currentScore = getValue('life')
+        const newScore = currentScore - 1
+        setValue('life', newScore)
+        if (newScore == 0) {
+            gameOver()
+        }
+    }
+}
+document.addEventListener('keyup', handleKeyUpEvent)
+
+function startGame() {
+    // random alphabet generate
+    const alphabet = getRandomAlphabet()
+    const current = document.getElementById('screen')
+    current.innerText = alphabet
+    addBgColor(alphabet)
 }
--- Js/utility.js
+++ Js/utility.js
@@ -8,3 +8,33 @@ function showElement(elementId) {
     const val = document.getElementById(elementId)
     val.classList.remove('hidden')
 }
+
+function getValue(elementId) {
+    const element = document.getElementById(elementId)
+    const text = element.innerText
+    const score = parseInt(text)
+    return score
+}
+
+function setValue(elementId, value) {
+    const element = document.getElementById(elementId)
+    element.innerText = value
+}
+
+function getRandomAlphabet() {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    const rand = alphabet.split('')
+    const randNum = Math.random() * 25
+    const index = Math.round(randNum);
+    const resValue = rand[index]
+    return resValue
+}
+
+function addBgColor(elementId) {
+    const element = document.getElementById(elementId)
+    element.classList.add('bg-orange-400')
+}
+function removeBgColor(elementId) {
+    const element = document.getElementById(elementId)
+    element.classList.remove('bg-orange-400')
+}
